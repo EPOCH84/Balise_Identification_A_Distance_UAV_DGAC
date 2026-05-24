56384449effa6938eeb97ed06164e5504b458779
MaaaX <maxime@maaax.com>
2026-05-24 23:17:36 +0200

    Add preference initialization in EEPROM

--- src/options.h
+++ src/options.h
@@ -26,6 +26,39 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
+//
+//#define OPTION_UPDATE_OTA				// Pour permettre une mise a jour OTA pour logiciel
+
+//#define OPTION_RECEIVER				// Pour inclure aussi le code récepteur (uniquement pour une carte à base ESP32 / ESP32C3)
+
+//#define OPTION_CELLULAR				// Pour envoyer dans un SMS la position avec une module GSM SIM800L (uniquement pour une carte à base ESP32 / ESP32C3)et incompatible avec fs_iBus
+										// Il faudra aussi définir GSM_RX et GSM_TX (voir exemples plus loin)
+
+//#define OPTION_STATISTICS				// Pour avoir une page de statisques,durée exécution, erreurs GPS, etc ...Utile en phase développement ..
+
+//#define OPTION_IBUS					// Pour envois informations GPS par telemetrie type FlySky / iBUS (incompatible avec repondeurGSM)
+										// Il faudra aussi définir iBus_RX et iBus_TX (voir exemples plus loin)
+
+
+//--------------------------------------------------------------------------------------------------------
+//  GPS TYPE
+//--------------------------------------------------------------------------------------------------------
+// Select only one GPS option.
+// If OPTION_GPS_QUECTEL and OPTION_GPS_UBLOX are not selected, the GPS chipset is supposed to be already configured and initialized.
+//#define OPTION_GPS_QUECTEL			// GPS Quectel L80 and GPS based on MediaTek MT3339 chipset
+#define OPTION_GPS_UBLOX				// GPS Beitian BN-220, BN-180, BN-880 and GPS based on u-blox M8030-KT chipset 
+
+
+
+//--------------------------------------------------------------------------------------------------------
+//  ESP32-C3 BOARD CONFIG
+//--------------------------------------------------------------------------------------------------------
+#ifdef TARGET_ESP32C3
+	#define OPTION_BOARD_PIN_LED			8	// Optional builtin LED pin
+	#define OPTION_BOARD_PIN_GPS_RX			0	// Connect the board D1 pin on the GPS TX
+	#define OPTION_BOARD_PIN_GPS_TX			1	// Connect the board D2 pin on the GPS RX
+	#define OPTION_BOARD_PIN_FACTORY_RESET	2
+#endif
 
 
 #endif // #ifndef OPTIONS_H
